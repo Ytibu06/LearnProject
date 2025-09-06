5013a2ca17b22a09781d140516ae705f81b923ea
subBen <dar@du.com>
2025-09-06 21:01:22 +0800

    finish--Glib_Thread

--- Thread/Glib/thread_pool.c
+++ Thread/Glib/thread_pool.c
@@ -7,19 +7,18 @@
 
 #define MAX_THREADS 10
 
-void *task_func(gpointer data, gpointer user_date){
+void task_func(gpointer data, gpointer user_data){
     int task_num = *(int *)data;
     free(data);
     printf("task_func: %d\n", task_num);
     sleep(task_num);
     printf("task_func: %d finish\n", task_num);
-    return NULL;
 }
 int main()
 {
 
     //创建线程池
-    GThreadPool *pool = g_thread_poll_new(task_func, NULL, MAX_THREADS, TRUE,NULL);
+    GThreadPool *pool = g_thread_pool_new(task_func, NULL, MAX_THREADS, TRUE,NULL);
     
     //添加任务
     for (size_t i = 0; i < 10; i++)
@@ -28,10 +27,10 @@ int main()
 
         *tmp = i + 1;
 
-        g_thread_poll_push(pool, tmp);
+        g_thread_pool_push(pool, tmp, NULL);
     }
     
-    g_thread_poll_free(pool,FALSE,TRUE);
+    g_thread_pool_free(pool,FALSE,TRUE);
     printf("main thread exit\n");
 
     return 0;

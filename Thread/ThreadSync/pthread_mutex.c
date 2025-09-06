91c4b02af5176c6374a077eed3ef16754ad62f1c
subBen <dar@du.com>
2025-09-06 20:25:43 +0800

    Add ThreadSync directory and update settings

--- Thread/ThreadSync/pthread_mutex.c
+++ Thread/ThreadSync/pthread_mutex.c
@@ -3,15 +3,13 @@
 #include <pthread.h>
 #include <unistd.h>
 
+#define THREAD_COUNT 20000
 //线程锁：互斥锁mutex
 
-
-#define THREAD_COUNT 20000
 // 初始化互斥锁
 static pthread_mutex_t counter_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *add_thread(void *arg){
-
     int * p = (int *)arg;
 
     //累计之前获取锁保证只有一个线程进行操作
@@ -22,23 +20,20 @@ void *add_thread(void *arg){
 
     return (void *)0;
 }
-int main(int argc, char *argv[]){
 
+int main(int argc, char *argv[]){
     pthread_t pid[THREAD_COUNT];
     int num = 0;
 
-    for (size_t i = 0; i < THREAD_COUNT; i++)
-    {
-        pthread_create(pid+i,NULL,add_thread,&num);
+    for (size_t i = 0; i < THREAD_COUNT; i++) {
+        pthread_create(pid+i, NULL, add_thread, &num);
     }
 
-    for (size_t i = 0; i < THREAD_COUNT; i++)
-    {
-        pthread_join(pid[i],NULL);
+    for (size_t i = 0; i < THREAD_COUNT; i++) {
+        pthread_join(pid[i], NULL);
     }
     
-    printf("num = %d\n",num);
+    printf("num = %d\n", num);
     
-
     return 0;
 }
